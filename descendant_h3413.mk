302a2d332669d32b19cc08ea98563fedcb77a920
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-05-10 13:21:58 +0200

    voyager

--- descendant_h3413.mk
+++ descendant_h3413.mk
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/sony/akatsuki/aosp_h9436.mk)
+$(call inherit-product, device/sony/voyager/aosp_h3413.mk)
 
-PRODUCT_NAME := descendant_h9436
-PRODUCT_DEVICE := akatsuki
-PRODUCT_MODEL := Xperia XZ3 Dual
+PRODUCT_NAME := descendant_h3413
+PRODUCT_DEVICE := voyager
+PRODUCT_MODEL := Xperia XA2 Plus
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
